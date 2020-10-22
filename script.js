bfb7fb88b906fa2a4488d094bbe2710bcc6e8bd0
Mariam Ali <alim12@csp.edu>
2020-10-22 16:07:49 -0500

    Updating

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-script
+let questions = [
+    {
+        question: "Question 1: blah blah blah question question?",
+        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+        answer: "blah 3",
+    },
+    {
+        question: "Question 1: blah blah blah question question?",
+        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+        answer: "blah 3",
+    },
+    {
+        question: "Question 1: blah blah blah question question?",
+        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+        answer: "blah 3",
+    },
+    {
+        question: "Question 1: blah blah blah question question?",
+        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+        answer: "blah 3",
+    },
+    {
+        question: "Question 1: blah blah blah question question?",
+        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+        answer: "blah 3",
+    },
+    {
+        question: "Question 1: blah blah blah question question?",
+        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+        answer: "blah 3",
+    },
+];
+
+function beginQuiz() {
+    let startbtnEl = document.querySelector("#startbtn");
+    let startTimer = document.querySelector("#timer");
+    let highscorebtnEl = document.querySelector("#highscorebtn");
+}
