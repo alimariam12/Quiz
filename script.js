9bc9b7222f7050846c2fa16ca855b4b41e8ff69d
Mariam Ali <alim12@csp.edu>
2020-10-24 13:24:12 -0500

    almost there

--- script.js
+++ script.js
@@ -1,16 +1,99 @@
-let start = document.querySelector("#startbtn")
-let questionEl = document.querySelector(".questions")
-let startPage = document.querySelector(".start-page")
+let startbtnEl = document.querySelector("#starterbtn")
+let startPage = document.querySelector(".firstcontainer")
+let questionPage = document.querySelector(".secondcontainer")
+let finalPage = document.querySelector(".lastcontainer")
+let highscorebtnEl = document.querySelector("#highscorebtn")
+let quizTimer = document.querySelector("#timer")
+let beginQuestions = document.querySelector("#quizstarts")
+let finalScore = document.querySelector("#getscore")
+let submitbtnEl = document.querySelector("#submit")
+let buttonA = document.querySelector("#A")
+let yourfinalScore = 0
+let totalSeconds = 100;
+let timer = totalSeconds;
+let nextQuestion
+let questionNumber = 0
+let answerChoices = document.querySelector("#question-text");
+let questionHead = document.getElementById("questions");
+let secondsLeft = (questions.length * 15 + 1);
 
 
-start.addEventListener("click", Quiz)
 
-function Quiz() {
+
+startbtnEl.addEventListener("click", startQuiz)
+let allQuestions = [
+    {
+        question: "Question 1: blah blah blah question question?",
+        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+        rightAnswer: "blah 3",
+    },
+    {
+        question: "Question 1: blah blah blah question question?",
+        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+        rightAnswer: "blah 3",
+    },
+    {
+        question: "Question 1: blah blah blah question question?",
+        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+        rightAnswer: "blah 3",
+    },
+    {
+        question: "Question 1: blah blah blah question question?",
+        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+        rightAnswer: "blah 3",
+    },
+    {
+        question: "Question 1: blah blah blah question question?",
+        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+        rightAnswer: "blah 3",
+    },
+]
+
+
+function startQuiz() {
     startPage.style.display = "none";
-    questionEl.style.display = "block";
+    questionPage.style.display = "block";
+    let startTime = setInterval(function() {
+        timer--; document.querySelector("#timer").innerHTML = timer;
+        if (timer === 0) {
+            console.log("timer")
+            clearInterval(startTime)
+            document.querySelector("#timer").innerHTML = "0"
+        }
+    }, 1000) ;
+    console.log("checking")
+    
+    startQuestions();
 }
-function showResults(){
+let answer;
+function startQuestions() {
+   // questionNumber++;
+   // answer = 
+console.log("quiz")
+
+    questionHead.textContent = allQuestions[questionNumber].question;
+    answerChoices.innerHTML = "";
+
+    let choices = allQuestions[questionNumber].options;
+    
+    for (let i = 0; i < choices.length; i++) {
+        let nextChoice = document.createElement("button");
+
+        nextChoice.textContent = choices[i]
+        answerBtn = answerChoices.appendChild(nextChoice).setAttribute("class", "p-3 m-1 btn btn-light btn-block");
+    }
+    
+}
+// if user statement = rightAnswer
+
+
+buttonA.addEventListener("click", finalgrade)
+
+function finalgrade() {
+    questionPage.style.display = "none";
+    finalPage.style.display = "block";
 
 }
+
 
 
