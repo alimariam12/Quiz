d36f3e18651aaad3fa0ffafff839830ac4aadb69
Mariam Ali <alim12@csp.edu>
2020-10-26 13:29:45 -0500

    need help

--- script.js
+++ script.js
@@ -7,49 +7,45 @@ let quizTimer = document.querySelector("#timer")
 let beginQuestions = document.querySelector("#quizstarts")
 let finalScore = document.querySelector("#getscore")
 let submitbtnEl = document.querySelector("#submit")
-let buttonA = document.querySelector("#A")
+let answerBtn = document.querySelector("#A")
 let yourfinalScore = 0
 let totalSeconds = 100;
 let timer = totalSeconds;
-let nextQuestion
+let nextQuestion 
 let questionNumber = 0
 let answerChoices = document.querySelector("#question-text");
 let questionHead = document.getElementById("questions");
 let secondsLeft = (questions.length * 15 + 1);
 
-
-
-
-startbtnEl.addEventListener("click", startQuiz)
 let allQuestions = [
     {
-        question: "Question 1: blah blah blah question question?",
-        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
-        rightAnswer: "blah 3",
+      question: "Question 1: blah blah blah question question?",
+      options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+      rightAnswer: "blah 3",
     },
     {
-        question: "Question 1: blah blah blah question question?",
-        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
-        rightAnswer: "blah 3",
+      question: "Question 1: blah blah blah question question?",
+      options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+      rightAnswer: "blah 3",
     },
     {
-        question: "Question 1: blah blah blah question question?",
-        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
-        rightAnswer: "blah 3",
+      question: "Question 1: blah blah blah question question?",
+      options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+      rightAnswer: "blah 3",
     },
     {
-        question: "Question 1: blah blah blah question question?",
-        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
-        rightAnswer: "blah 3",
+      question: "Question 1: blah blah blah question question?",
+      options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+      rightAnswer: "blah 3",
     },
     {
-        question: "Question 1: blah blah blah question question?",
-        options: ["blah 1", "blah 2", "blah 3", "blah 4"],
-        rightAnswer: "blah 3",
+      question: "Question 1: blah blah blah question question?",
+      options: ["blah 1", "blah 2", "blah 3", "blah 4"],
+      rightAnswer: "blah 3",
     },
 ]
 
-
+startbtnEl.addEventListener("click", startQuiz)
 function startQuiz() {
     startPage.style.display = "none";
     questionPage.style.display = "block";
@@ -69,37 +65,29 @@ let answer;
 function startQuestions() {
     questionHead.textContent = allQuestions[questionNumber].question;
     answerChoices.innerHTML = "";
-
+  
     let choices = allQuestions[questionNumber].options;
-    
+  
     for (let i = 0; i < choices.length; i++) {
-        let nextChoice = document.createElement("button");
-
-        nextChoice.textContent = choices[i]
-        answerBtn = answerChoices.appendChild(nextChoice).setAttribute("class", "p-3 m-1 btn btn-light btn-block");
+      let nextChoice = document.createElement("button");
+  
+      nextChoice.textContent = choices[i];
+      nextChoice.setAttribute("class", "p-3 m-1 btn btn-light btn-block answer");
+      nextChoice.addEventListener("click", checkChoice);
+      answerChoices.appendChild(nextChoice);
     }
-    checkChoice(choices)();
-    
-}
-
-function checkChoice(choices) {
-    // Get the current answer from allQuestions[questionNumber]
-
+    // checkChoice(choices)();
+  }
+  
+  function checkChoice(choices) {
+    console.log("hi");
     if (choices === answer) {
-        allQuestions[questionNumber].rightAnswer
+      allQuestions[questionNumber].rightAnswer;
     } else {
-        // Our choice is incorrect...
     }
-}
-// if user statement = rightAnswer
-nextChoice.onclick = function() { checkChoice(choices[i]) }
-
-buttonA.addEventListener("click", finalgrade)
-
-function finalgrade() {
-    questionPage.style.display = "none";
-    finalPage.style.display = "block";
-
+  }
+function checkChoice(event) {
+  console.log(event.target);
 }
 
 
